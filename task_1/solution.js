45215817bf7ab9d8b5737c1d6acde1a1365b5921
Artem Klem <tanyavibe@gmail.com>
2022-11-04 21:12:44 +0300

    внесли имзмения задание 1.1.

--- task_1/solution.js
+++ task_1/solution.js
@@ -3,16 +3,15 @@ function addInCartMessage(title, price) {
     let productPrice = price;
 
     // Задание №1.1. Оповещение о добавлении в корзину
-
-    // создайте переменную message
-    // присвойте переменной message значение переменной productName
-    // добавьте в конец переменной message пробел
-    // добавьте в конец переменной message текст 'за'
-    // добавьте в конец переменной message пробел
-    // добавьте в конец переменной message значение переменной productPrice
-    // добавьте в конец переменной message пробел
-    // добавьте в конец переменной message текст 'теперь в корзине!'
-    // выведите переменную message в консоль
+    let message;
+    message = productName;
+    message = message + " ";
+    message = message + "за";
+    message = message + " ";
+    message = productPrice;
+    message = message +  " ";
+    message = message + "теперь в корзине";
+    console.log (message);
 
     // Конец решения задания №1.1.
 
