3acd029ecbe9d3dcf763c3dc946f978683ed4e27
Artem Klem <tanyavibe@gmail.com>
2022-11-05 16:30:32 +0300

    task 3 has been changed

--- task_1/solution.js
+++ task_1/solution.js
@@ -28,7 +28,6 @@ function addInCartCountChange(value) {
     newValue = oldValue;
     newValue+=1
 
-
     // создайте переменную newValue
     // присвойте ей значение oldValue
     // увеличьте значение newValue на 1
@@ -43,6 +42,12 @@ function addInCartSumChange(sum, delta) {
     let difference = delta;
 
     // Задание №1.3. Увеличение суммы в корзине
+
+    let newSum;
+        newSum = oldSum + difference;
+    let newSumText;
+        newSumText = newSum + ' ₽';
+        newSumText = `${newSum} ₽`;
 
     // создайте переменную newSum и присвойте ей сумму oldSum и difference
     // создайте переменную newSumText и присвойте ей результат склеивания переменной newSum и текста ' ₽'
