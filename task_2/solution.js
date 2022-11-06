f8034533b182fd0bb79cd992e8379f3ab49e3646
ArtemKlem <selivn_78@mail.ru>
2022-11-06 20:29:34 +0300

    fix test error

--- task_2/solution.js
+++ task_2/solution.js
@@ -4,17 +4,15 @@ function calcShipping(sum, min, shipping) {
     let shippingPrice = shipping; // стоимость доставки
 
     // Задание №2.1. Рассчитать доставку
-
     let shippingSum; // создайте переменную shippingSum
-     if (productsSum == 0 ) {  // если productsSum равно 0,
-        shippingSum = 0;      // то shippingSum присвоить значение 0
-     }
-     if (productsSum >= freeShippingMinSum) { // если productsSum Больше или равна freeShippingMinSum,
-        shippingSum = 0;  // то shippingSum присвоить значение 0
-     }
-     else if (productsSum > 0 || productsSum < freeShippingMinSum); // если productsSum больше 0 и меньше freeShippingMinSum,
-        shippingSum = shippingPrice; // то shippingSum присвоить значение shippingPrice
 
+    if (productsSum == 0 || productsSum >= freeShippingMinSum) { // если productsSum равно 0, больше или равно freeShippingMinSum,
+        shippingSum = 0; // то shippingSum присвоить значение 0
+    } 
+    else if (productsSum > 0 || productsSum < freeShippingMinSum) { // если productsSum больше 0 и меньше freeShippingMinSum,
+        shippingSum = shippingPrice; // то shippingSum присвоить значение shippingPrice
+    }
+  
     // Конец решения задания №2.1.
 
     return shippingSum;
